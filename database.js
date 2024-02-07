3728fdc2d3cd0afefb323d6051a620a14a1635de
Danielzv98 <159276933+Danielzv98@users.noreply.github.com>
2024-02-07 10:48:53 -0400

    Add files via upload

---
+++ database.js
@@ -0,0 +1,32 @@
+const sliderDb = [
+  {
+    title:"Titulo de ejemplo uno",
+    src:"C:\Users\Daniel\Desktop\Daniel UPTAEB\Universidad UPTAEB\TRAYECTO I\PROYECTO\Pagina web de Proyecto\Evidencia\1706235328745.jpg"
+  },
+  {
+    title:"Titulo de ejemplo dos",
+    src:"n1.jpg"
+  },
+  {
+    title:"Titulo de ejemplo tres",
+    src:"https://source.unsplash.com/random/?cars"
+  },
+  {
+    title:"Titulo de ejemplo cuatro",
+    src:"https://source.unsplash.com/random/?bikes"
+  },
+  {
+    title:"Titulo de ejemplo cinco",
+    src:"https://source.unsplash.com/random/?houses"
+  },
+  {
+    title:"Titulo de ejemplo seis",
+    src:"https://source.unsplash.com/random/?peets"
+  },
+  {
+    title:"Titulo de ejemplo siete",
+    src:"https://source.unsplash.com/random"
+  },
+
+]
+
